63ed94631d25fd0c00d6e63879d577c306662da2
AveiaPodre <av_jr@outlook.com.br>
2022-12-08 22:58:44 -0300

    so falta o ngc la

---
+++ Estoque.java
@@ -0,0 +1,62 @@
+package averinaldoJunior.estoque;
+
+import java.util.ArrayList;
+
+public class Estoque {
+    private ArrayList<Produto> produtos = new ArrayList<>();
+
+    public void incluir(Produto p) {
+        this.produtos.add(p);
+    }
+
+    public void comprar(int cod, int quant, double preco) {
+        for (Produto produto : produtos) {
+            if (produto.getCodigo() == cod) {
+                produto.compra(quant, preco);
+                return;
+            }
+        }
+    }
+
+    public double vender(int cod, int quant) {
+        for (Produto produto : produtos) {
+            if (produto.getCodigo() == cod) {
+                return produto.venda(quant);
+            }
+        }
+
+        return -1;
+    }
+
+    public int quantidade(int cod) {
+        for (Produto produto : produtos) {
+            if (produto.getCodigo() == cod) {
+                return produto.getQuantidade();
+            }
+        }
+
+        return -1;
+    }
+
+    public Fornecedor fornecedor(int cod) {
+        for (Produto produto : produtos) {
+            if (produto.getFornecedor().getCnpj() == cod) {
+                return produto.getFornecedor();
+            }
+        }
+
+        return null;
+    }
+
+    public ArrayList<Produto> estoqueAbaixoDoMinimo() {
+        ArrayList<Produto> produtosAbaixoDoMinimo = new ArrayList<>();
+
+        for (Produto produto : produtos) {
+            if (produto.produtoAbaixoDoMinimo()) {
+                produtosAbaixoDoMinimo.add(produto);
+            }
+        }
+
+        return produtosAbaixoDoMinimo;
+    }
+}
---
+++ Produto.java
@@ -0,0 +1,67 @@
+package averinaldoJunior.estoque;
+
+public class Produto {
+    private int codigo;
+    private String descricao;
+    private double precoDeCompra;
+    private double precoDeVenda;
+    private double lucro;
+    private int quantidade;
+    private int estoqueMinimo;
+
+    private Fornecedor fornecedor;
+
+    //o codigo do produto novo não pode ser igual ao de um ja existente
+    public Produto(int cod, String desc, int min, double lucro, Fornecedor forn){
+        this.codigo = cod;
+        this.descricao = desc;
+        this.estoqueMinimo = min;
+        this.lucro = lucro;
+        this.fornecedor = forn;
+    }
+
+    public int compra(int quant, double val){
+        if(quant <= 0 || val <= 0){
+            return -1;
+        }
+
+        this.setPrecoDeCompra(quant, val);
+        this.quantidade += quant;
+
+        return this.quantidade;
+    }
+
+    public double venda(int quant){
+        if(quant <= 0 || quant>this.quantidade){
+            return -1;
+        }
+        this.quantidade -= quant;
+
+        return quant * this.precoDeVenda;
+    }
+
+    public void setPrecoDeCompra(int quantidade, double val){
+        this.precoDeCompra = (this.quantidade * this.precoDeCompra + quantidade * val) / (this.quantidade + quantidade);
+        this.precoDeVenda = this.precoDeCompra + (this.precoDeCompra * this.lucro);
+    }
+
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public boolean produtoAbaixoDoMinimo(){
+        return this.quantidade < this.estoqueMinimo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
